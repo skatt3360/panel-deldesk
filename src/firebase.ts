7ec064d9ce5fc71276d6617ec09727464a725f16
Szymon Karaszewski <szymon.karaszewski@MacBook-Air-Szymon.local>
2026-04-09 20:37:43 +0200

    fix: move firebase config to env variables to pass netlify secrets scan

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,14 +3,13 @@ import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCuCXGLol577LdJBFzkNWky27eDJdaBF0w',
-  authDomain: 'panel-helpdesk-ed3f1.firebaseapp.com',
-  databaseURL: 'https://panel-helpdesk-ed3f1-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'panel-helpdesk-ed3f1',
-  storageBucket: 'panel-helpdesk-ed3f1.firebasestorage.app',
-  messagingSenderId: '384650483840',
-  appId: '1:384650483840:web:7a8db9ea172423046aee65',
-  measurementId: 'G-4CH63EKLVH',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
